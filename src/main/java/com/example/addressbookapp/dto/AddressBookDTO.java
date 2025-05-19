0521360e01e18d97ecff7cf3bcc899a02b74f45f
kartikkkkg <kartikgupta.jaipur@gmail.com>
2025-05-19 17:28:10 +0530

    feat: UC3 - Add DTO and Model classes for Address Book

---
+++ src/main/java/com/example/addressbookapp/dto/AddressBookDTO.java
@@ -0,0 +1,19 @@
+package com.example.addressbookapp.dto;
+
+public class AddressBookDTO {
+    public String name;
+    public String address;
+
+    public AddressBookDTO() {
+    }
+
+    public AddressBookDTO(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "AddressBookDTO [name=" + name + ", address=" + address + "]";
+    }
+}
---
+++ src/main/java/com/example/addressbookapp/model/AddressBookData.java
@@ -0,0 +1,46 @@
+package com.example.addressbookapp.model;
+import com.example.addressbookapp.dto.AddressBookDTO;
+
+public class AddressBookData {
+    private int id;
+    private String name;
+    private String address;
+
+    public AddressBookData() {
+    }
+
+    public AddressBookData(int id, AddressBookDTO dto) {
+        this.id = id;
+        this.name = dto.name;
+        this.address = dto.address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "AddressBookData [id=" + id + ", name=" + name + ", address=" + address + "]";
+    }
+}
